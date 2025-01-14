68261b99d1964c78c5d6642c59a2c57b01c05a93
Vaibhav1Kathait <145695237+Vaibhav1Kathait@users.noreply.github.com>
2025-01-15 00:17:57 +0530

    Update meta.js

--- SimonSays/meta.js
+++ SimonSays/meta.js
@@ -1,32 +1,41 @@
-let gameseq=[];
-let userseq=[];
+let gameseq = [];
+let userseq = [];
 let started = false;
-let level=0;
-let xpass=0;
-let btns=["yellow","red","purple","green"];
+let level = 0;
+let highestScore = 0; 
+let currentScore = 0; 
+let btns = ["yellow", "red", "purple", "green"];
 let h2 = document.querySelector("h2");
-document.addEventListener("keypress", function() {
-    if(started==false) {
+let highestScoreDisplay = document.querySelector("#highest-score");
+let currentScoreDisplay = document.querySelector("#current-score");
+
+document.addEventListener("keypress", function () {
+    if (started == false) {
         console.log("game started");
         started = true;
         levelup();
     }
 });
+
 function btnFlash(btn) {
     btn.classList.add("flash");
-    setTimeout(function() {
-       btn.classList.remove("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
     }, 250);
 }
+
 function userFlash(btn) {
     btn.classList.add("userflash");
-    setTimeout(function() {
-       btn.classList.remove("userflash");
+    setTimeout(function () {
+        btn.classList.remove("userflash");
     }, 250);
 }
+
 function levelup() {
     userseq = [];
     level++;
+    currentScore = level; // Update current score with the current level
+    currentScoreDisplay.innerText = `Current Score: ${currentScore}`; // Update current score display
     h2.innerText = `Level ${level}`;
     let randIdx = Math.floor(Math.random() * 3);
     let randomColor = btns[randIdx];
@@ -37,41 +46,50 @@ function levelup() {
     console.log(randomColor);
     btnFlash(randbtn);
 }
+
 function checkans(idx) {
-   if(userseq[idx] ===gameseq[idx])  {
-      if(userseq.length == gameseq.length){
-          setTimeout(levelup,1000);
-      }
+    if (userseq[idx] === gameseq[idx]) {
+        if (userseq.length == gameseq.length) {
+            setTimeout(levelup, 1000);
+        }
 
-   } else {
-      h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br> Press any key to start, `;
-      document.querySelector("body").style.backgroundColor = "red";
-      setTimeout(function () {
-        document.querySelector("body").style.backgroundColor = "white";
-      }, 150);
-      reset();
-   }
+    } else {
+        h2.innerHTML = `Game Over! Your score was <b>${currentScore}</b> <br> Press any key to start, `;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function () {
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 150);
 
+        // Update highest score if necessary
+        if (currentScore > highestScore) {
+            highestScore = currentScore;
+            highestScoreDisplay.innerText = `Highest Score: ${highestScore}`;
+        }
+
+        reset();
+    }
 }
+
 function btnPress() {
     console.log(this);
-    let btn=this;
+    let btn = this;
     userFlash(this);
 
     usercolor = btn.getAttribute("id");
     userseq.push(usercolor);
-    checkans(userseq.length-1);
- }
+    checkans(userseq.length - 1);
+}
 
-    let allBtns = document.querySelectorAll(".btn");
-    for(btn of allBtns) {
-        btn.addEventListener("click",btnPress);
-    }
+let allBtns = document.querySelectorAll(".btn");
+for (btn of allBtns) {
+    btn.addEventListener("click", btnPress);
+}
 
-    function reset() {
-        started  = false;
-        gameseq = [];
-        userseq =  [];
-        level = 0;
-    }
-    
+function reset() {
+    started = false;
+    gameseq = [];
+    userseq = [];
+    level = 0;
+    currentScore = 0; // Reset current score
+    currentScoreDisplay.innerText = `Current Score: 0`; // Reset current score display
+}
